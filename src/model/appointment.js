b46d73be2cbafb5408f3790ddfd452815bb1711d
amal448 <amalthomas333444@gmail.com>
2023-06-27 11:46:13 +0530

    message

--- src/model/appointment.js
+++ src/model/appointment.js
@@ -1,12 +1,17 @@
-const { ObjectId } = require("mongodb");
 const mongoose=require("mongoose");
 
 const Appointment=new mongoose.Schema({
-    userId:ObjectId,
-    doctorId:ObjectId,
+    firstName:String,
+    lastName:String,
+    email:String,
+    mobile:String,
+    gender:String,
+    dob:String,
+    appointmentTime:String,
+    appointmentDate:Date,
     doctorName:String,
-    doctorImage:String,
     department:String,
+<<<<<<< HEAD
     date:Date,
     time:String,
     price:Number,
@@ -19,4 +24,12 @@ const Appointment=new mongoose.Schema({
 { timestamps: true }
 )
 const Appointments=mongoose.model("appointment",Appointment)
+=======
+    age:String,
+    address:String,
+    status:String,
+    userId:String
+})
+const Appointments=mongoose.model("appointment")
+>>>>>>> parent of fe58e5b (payment started)
 module.exports =Appointments
--- src/routes/doctorRoute.js
+++ src/routes/doctorRoute.js
@@ -7,10 +7,13 @@ const router=express.Router();
 // router.post('/apply-post',doctorController.ApplyforDoctor)
 router.post('/login',doctorController.login)
 // router.get('/get-all-doctors',doctorAuth,doctorController.getAllDoctors)
+<<<<<<< HEAD
 router.put('/update-timeSlot',doctorAuth,doctorController.addDoctorTimeSlot)
 router.get('/get-patients',doctorAuth,doctorController.getPatients)
 router.get('/:id',doctorController.getDoctor)
 
 
 
+=======
+>>>>>>> parent of fe58e5b (payment started)
 module.exports=router;
