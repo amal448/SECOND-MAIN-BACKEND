a8b924cd105e0de33f5c52a8e29643fc6f081af5
amal448 <amalthomas333444@gmail.com>
2023-09-11 17:02:11 +0530

    Checking the changes

--- src/routes/doctorRoute.js
+++ src/routes/doctorRoute.js
@@ -10,6 +10,7 @@ router.post('/login',doctorController.login)
 router.put('/update-timeSlot',doctorAuth,doctorController.addDoctorTimeSlot)
 router.get('/get-patients',doctorAuth,doctorController.getPatients)
 router.get('/doctor/:doctorId',doctorAuth,doctorController.getDoctor)
+router.get('/user/:userId',doctorAuth,doctorController.getUser)
 router.get('/Appointmentdata',doctorAuth,doctorController.getAppointments)
 router.get('/getFullProfit/:doctorId',doctorAuth,doctorController.getFullPayment) 
 router.get('/monthly-report/:doctorId',doctorAuth,doctorController.monthlyReport)
@@ -17,7 +18,6 @@ router.get('/weekly-report',doctorAuth,doctorController.weeklyReport)
 router.get('/monthly-report',doctorAuth,doctorController.monthlyReport);
 router.get('/daily-report',doctorAuth,doctorController.dailyReport);
 router.get('/yearly-report',doctorAuth,doctorController.getYearlyReport);
-router.get('/user/:userId',doctorAuth,doctorController.getUser)
 
 router.get('/prescriptions/:userId',doctorAuth,doctorController.prescriptions);
 router.get('/singleAppointment/:userId',doctorAuth,doctorController.singleAppointment)
